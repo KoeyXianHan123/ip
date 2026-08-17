ad77a56f2766e2402c1bbf863313459fbf177dca
KoeyXianHan123 <kxh3159@gmail.com>
2026-08-17 20:46:00 +0800

    Complete Level-0

---
+++ src/main/java/Nova.java
@@ -0,0 +1,16 @@
+/**
+ * Starts the Nova chatbot application.
+ */
+public class Nova {
+    public static void main(String[] args) {
+        String banner = " _   _                  \n"
+                + "| \\ | | _____   ____ _ \n"
+                + "|  \\| |/ _ \\ \\ / / _` |\n"
+                + "| |\\  | (_) \\ V / (_| |\n"
+                + "|_| \\_|\\___/ \\_/ \\__,_|\n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Nova.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
